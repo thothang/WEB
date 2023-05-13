bcd12865c909357e6d0a7bd4c3e7defc5f985f9d
thothang <tn28112017@gmail.com>
2023-05-13 22:07:21 +0700

    chưa xong :<<

---
+++ Admin/slider.php
@@ -0,0 +1,24 @@
+<section class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li><a href="">Danh mục</a>
+                    <ul>
+                        <li><a href="cartegoryadd.php">Thêm danh mục</a></li>
+                        <li><a href="cartegorylist.php">Danh sách danh mục</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Loại sản phẩm</a>
+                    <ul>
+                        <li><a href="">Thêm loại sản phẩm</a></li>
+                        <li><a href="">Danh loại sản phẩm</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Sản phẩm</a>
+                    <ul>
+                        <li><a href="">Thêm sản phẩm</a></li>
+                        <li><a href="">Danh sách sản phẩm</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+       
